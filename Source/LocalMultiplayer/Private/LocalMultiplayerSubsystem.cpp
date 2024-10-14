17aafb479c2116d747b143b2090fbb71b3fd515f
Corentinrmt <corentin.remot@gmail.com>
2024-10-14 15:01:46 +0200

    [ADD] assign player to controller to input type part 1

---
+++ Source/LocalMultiplayer/Private/LocalMultiplayerSubsystem.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LocalMultiplayerSubsystem.h"
+
+void ULocalMultiplayerSubsystem::CreateAndInitPlayer(ELocalMultiplayerInputMappingType MappingType)
+{
+	
+}
+
